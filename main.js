9583b0fe583bdfaed2b331666859fda94379e761
surze <angpaono1@hotmail.com>
2021-02-21 15:05:49 +0700

    [FINISHED GROUPWORK4] - Click To Big Photos

--- main.js
+++ main.js
@@ -1,23 +1,79 @@
 const app = {
     data() {
         return {
-        photos:[
-            {url: 'images/photo1.jfif', alt:'grow tower under grey sky'},
-            {url: 'images/photo2.jfif', alt:'Green grass field during daytime'},
-            {url: 'images/photo3.jfif', alt:'gray concrete castle on green grass field'}
-        ],
-        
+            photos: [{
+                    url: 'images/photo1.jfif',
+                    alt: 'grow tower under grey sky',
+                    show: true
+                },
+                {
+                    url: 'images/photo2.jfif',
+                    alt: 'Green grass field during daytime',
+                    show: true
+                },
+                {
+                    url: 'images/photo3.jfif',
+                    alt: 'gray concrete castle on green grass field',
+                    show: true
+                }
+            ],
+            searchbtn: true,
+            cancelbtn: false,
+            inputbtn: false,
+            searchphoto: '',
+            popup: false,
+            bigphos: "",
+
         }
     },
     methods: {
-        toggleheart(index){
+        toggleheart(index) {
             this.photos[index].done = !this.photos[index].done
+        },
+        mouseclicksearch() {
+            this.searchbtn = false
+            this.cancelbtn = true
+            this.inputbtn = true
+        },
+        mouseclickcancel() {
+            this.cancelbtn = false
+            this.searchbtn = true
+            this.inputbtn = false
+            for (i = 0; i < this.photos.length; i++) {
+                this.photos[i].show = true;
+            }
+        },
+        searchingphoto() {
+            if (this.searchphoto) {
+                for (i = 0; i < this.photos.length; i++) {
+                    if (this.photos[i].alt.toLowerCase().includes(this.searchphoto.toLowerCase())) {
+                        this.photos[i].show = true;
+                    } else {
+                        this.photos[i].show = false;
+                    }
+                }
+            }
+        },
+        popupfunc(index){
+            this.popup = true;
+            this.bigphos = this.photos[index].url;
+        }
+
+    },
+
+    computed: {
+        countphoto() {
+            return this.photos.filter(t => !t.done).length
+        },
+        checkItems(){
+            let count = 0
+            for(let i = 0; i< this.photos.length;i++){
+                if(this.photos[i].show == true) count++;
+            }
+            if(count == 0) return "No photo.";
         }
     },
-    computed: {
-        countphoto(){
-            return this.photos.filter( t => !t.done ).length
-        }
-    }
+
+
 }
 Vue.createApp(app).mount('#container')
